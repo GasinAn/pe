c51fc14da86367d48f2bf85a02ed142351ea0580
GasinAn <Gasin185@163.com>
2023-11-14 00:55:40 +0800

    add O3b ini

---
+++ O3b_ini/sort_ini.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+ps = [p for p in Path('./Config_INIs_GR').iterdir()]
+for p in ps:
+    with p.open('r') as f:
+        inis = f.readlines()
+    inis.sort()
+    with p.open('w') as f:
+        for ini in inis:
+            if ini != '\n':
+                f.write(ini)
